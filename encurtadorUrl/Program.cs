e23876674a976e7f9b71e154d0b7603be0f74d94
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2023-02-14 15:04:24 -0300

    Testes da linguagem

--- encurtadorUrl/Program.cs
+++ encurtadorUrl/Program.cs
@@ -14,4 +14,26 @@ decimal numberDecimal = 5.3m;
 char isActive = 'y';
 bool isActiveBool = false;
 
+// Estrutura de controle de fluxo
+
+Console.WriteLine("Digite aqui o valor do produto:");
+
+var valorProduto = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor que você possui:");
+
+var valorQuePossui = int.Parse(Console.ReadLine());
+
+var diferencaValor = valorQuePossui - valorProduto ;
+
+if (diferencaValor < 0)
+{
+    Console.WriteLine("Você não pode comprar esse produto:");
+} else if(diferencaValor == 0)
+{
+    Console.WriteLine("Você pode pagar por esse produto");
+}else
+{
+    Console.WriteLine("Você pode comprar esse produto e sobrará " + diferencaValor);
+}
 
