ec5d9e2f1ff78eaa374331b839c677c7eba9b78f
Rodrigo Lopes <rodrigoczlopes@gmail.com>
2023-02-14 14:27:26 -0300

    Adicionar arquivos de projeto.

---
+++ encurtadorUrl/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite seu nome.");
+
+var nameInput = Console.ReadLine();
+
+Console.WriteLine("Olá " + nameInput);
